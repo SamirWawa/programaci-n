c5b5690e6a7cdb4e7a137377170a6eb1cb0a8a09
SamirWawa <116147764+SamirWawa@users.noreply.github.com>
2022-11-28 18:23:15 +0100

    Add files via upload

---
+++ SamirWawa.tema5.2-bloque3/Ejercicio8.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[][] matriz=new int[][]
+        {
+        };
+        int filas=default;
+        Console.WriteLine("Cuantas filas");
+        filas=int.Parse(Console.ReadLine());
+        for(int i=0;i<filas;i++)
+        {
+            Console.WriteLine("Cuantos valores vas a introducir?");
+            int valores=int.Parse(Console.ReadLine());
+            for(int j=0;j<valores;j++)
+            {
+                Console.WriteLine("Dime valores");
+                matriz[i][j]=int.Parse(Console.ReadLine());
+            }
+        }
+
+        for(int i=0;i<matriz.Length;i++)
+        {
+            for(int j=0;j<matriz[i].Length;i++)
+            {
+                Console.Write(matriz[i][j]+" ");
+            }
+            Console.WriteLine("");
+        }
+        
+    }
+}
