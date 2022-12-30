2d106550c2495a39eaced79fa5038b101fefc3a6
SamirWawa <116147764+SamirWawa@users.noreply.github.com>
2022-12-30 19:13:59 +0100

    Add files via upload

---
+++ TrabajoNavidadSamirWawa/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading;
+internal class Program
+{
+    const int ANCHO_CONSOLA = 80;
+        const int ALTO_CONSOLA = 24;
+        const int FILAS = 6;
+        const int COLUMNAS = 7;
+        const int SEP_HUECOS_HORIZONTAL = 4;
+        const int SEP_HUECOS_VERTICAL = 2;
+        const int FILA_TABLERO = ALTO_CONSOLA - FILAS * SEP_HUECOS_VERTICAL - 2;
+        const int COLUMNA_TABLERO = ANCHO_CONSOLA / 2 - COLUMNAS * SEP_HUECOS_HORIZONTAL / 2;
+		const int FILA_FICHAS = FILA_TABLERO;
+		const int COLUMNA_FICHAS = COLUMNA_TABLERO + 2;
+        const int NUM_FICHAS_EN_LINEA = 4;
+        const int PAUSA_CAIDA_FICHA_MS = 75;
+    public enum TipoFicha { SinFicha, FichaJugador1, FichaJugador2 }
+    public enum Turno { Jugador1, Jugador2 }
+
+    public static (TipoFicha[,] fichasEnJuego, (string, int)[] jugadores, Turno turno) InicializaJuego()
+    {
+        InicializaFichasEnJuego();
+        InicializaJugadores();
+        Turno turno = Turno.Jugador1;
+    }
+    public static TipoFicha[,] InicializaFichasEnJuego()
+    {
+        TipoFicha[,] Tablero = new TipoFicha[FILAS, COLUMNAS]
+        {
+            {TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha},
+            {TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha},
+            {TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha},
+            {TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha},
+            {TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha},
+            {TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha},
+            {TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha, TipoFicha.SinFicha},
+        };
+        return Tablero;
+    }
+    public static string[] InicializaJugadores()
+    {
+        string [] jugadores= new string[2];
+        Console.WriteLine("Introduzca nombre del jugador 1");
+        jugadores[0]= Console.ReadLine();
+        Console.WriteLine("Introduzca nombre del jugador 2");
+        jugadores[1]= Console.ReadLine();
+        return jugadores;
+    }
+    private static void Main(string[] args)
+    {
+        InicializaJuego();
+        for( int i=0; i<Tablero.GetLength(0); i++ )
+        {
+            for( int j=0; j< Tablero.GetLength(1); j++ )
+            {
+                Console.Write(Tablero[i,j]);
+            }
+        }
+    }
+}
