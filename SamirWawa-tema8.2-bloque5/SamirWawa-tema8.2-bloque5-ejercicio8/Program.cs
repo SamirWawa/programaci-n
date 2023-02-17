054993ec73f73aa2134fc0843a4fc81e43d5c134
SamirWawa <samir.wawa@iesdoctorbalmis.com>
2023-02-17 12:36:13 +0100

    Ejercicio8Flujos

---
+++ SamirWawa-tema8.2-bloque5/SamirWawa-tema8.2-bloque5-ejercicio8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+internal class Program
+{
+    private static void BuscarEnCadena(string cadena, string palabra)
+    {
+        if(Regex.IsMatch(cadena, palabra))
+            Console.WriteLine("Existe la palabra en el fichero");
+    }
+
+    private static void BuscarEnFichero(string ruta, string palabra)
+    {
+        FileStream f = new FileStream(ruta, FileMode.Open, FileAccess.Read);
+        BinaryReader br = new BinaryReader(f, System.Text.Encoding.UTF8);
+        string cadena = br.ReadString();
+        BuscarEnCadena(cadena, palabra);
+        br.Close();
+    }
+
+    private static void Main(string[] args)
+    {
+        string ruta = default;
+        string palabra = default;
+
+        Console.WriteLine("Define ruta");
+        ruta = Console.ReadLine();
+        Console.WriteLine("palabra a buscar");
+        palabra = Console.ReadLine();
+
+        BuscarEnFichero(ruta, palabra);
+    }
+}
