e7a125d4e5f387d185dbc55746824c35f1ad79f4
Samir Wawa <sawawaasnani@iesdoctorbalmis.com>
2023-03-02 19:27:36 +0100

    Ejercicios9.1

---
+++ SamirWawa-tema9.1-bloque6/SamirWawa-tema9.1-bloque6-ejercicio1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.RegularExpressions;
+using Spectre.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        FileStream f = new FileStream("datos.csv", FileMode.Open, FileAccess.Read);
+        StreamReader b = new StreamReader(f, System.Text.Encoding.UTF8);
+        var tabla = new Table();
+        var timp = new List<(string numero, double valor)> { };
+        var vViento = new List<(string numero, double valor)> { };
+        bool c = true;
+        string patron = @"(\d{4})(\d{2})(\d{2})(T)(\d{2})(\d{2})";
+
+        while(!b.EndOfStream)
+        {
+            string[] s = b.ReadLine().Split(',');
+                if (c)
+                {
+                    for (int i = 0; i < s.Length; i++)
+                        tabla.AddColumn($"[green]{s[i]}[/]");
+                    c= false;
+                }
+                else
+                {
+                    for (int i = 0; i < s.Length;i= i+5)
+                    {
+                            Match coincidencia = Regex.Match(s[i+6], patron);
+                            int año = int.Parse(coincidencia.Groups[1].Value);
+                            int mes = int.Parse(coincidencia.Groups[2].Value);
+                            int dia = int.Parse(coincidencia.Groups[3].Value);
+                            int hora = int.Parse(coincidencia.Groups[5].Value);
+                            int min = int.Parse(coincidencia.Groups[6].Value);
+                            DateTime d = new DateTime(año, mes, dia, 0, hora, min);
+                            tabla.AddRow(d.ToString(), s[i+7], s[i+8], s[i+9], s[i+10]);
+                    }
+                    timp.Add((s[0], Convert.ToDouble(s[1].Replace('.', ','))));
+                    vViento.Add((s[0], Convert.ToDouble(s[3].Replace('.', ','))));
+                }
+        }
+        b.Close();
+        AnsiConsole.Write(tabla);
+        AnsiConsole.Write(new BarChart()
+           .Width(60)
+           .Label($"[green bold underline]Temperatura[/]")
+           .CenterLabel()
+           .AddItems(timp, (item) => new BarChartItem(
+        item.numero, item.valor, Color.Pink1)));
+        AnsiConsole.Write(new BarChart()
+           .Width(60)
+           .Label($"[green bold underline]Velocidad Viento[/]")
+           .CenterLabel()
+           .AddItems(vViento, (item) => new BarChartItem(
+        item.numero, item.valor, Color.Purple)));
+        
+        
+    }
+}
---
+++ SamirWawa-tema9.1-bloque6/SamirWawa-tema9.1-bloque6-ejercicio3/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Estadistica
+{
+    public class Promedio
+    {
+        private double[] nums;
+        public Promedio(double[] nums)
+        {
+            this.nums = nums;
+        }
+        public double Media()
+        {
+            double media = default;
+            for (int i = 0; i < nums.Length; i++)
+                media += nums[i];
+            return media / nums.Length;
+        }
+        public double Mayor()
+        {
+            Array.Sort(nums);
+            return nums[nums.Length - 1];
+        }
+        public double Menor()
+        {
+            Array.Sort(nums);
+            return nums[0];
+        }
+        public double Rango()
+        {
+            Array.Sort(nums);
+            return nums[nums.Length - 1] - nums[0];
+        }
+        public double Mediana()
+        {
+            if (nums.Length % 2 == 0)
+            {
+                Array.Sort(nums);
+                return (nums[(nums.Length - 1) / 2] + nums[(nums.Length - 1) / 2 + 1]) / 2;
+            }
+            else
+                return nums[nums.Length / 2];
+        }
+        public double Moda()
+        {
+            double moda = 0;
+            double n = 0;
+            double o = 0;
+            double p = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                p = nums[i];
+                for (int j = 0; j < nums.Length; j++)
+                    if (j != i && nums[i] == nums[j])
+                        n++;
+                if (o < n)
+                {
+                    o = n;
+                    moda = nums[i];
+                }
+                n = 0;
+            }
+            return moda;
+        }
+    }
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            double[] d = {3, 7, 5, 7, 4, 3};
+            Promedio p = new Promedio(d);
+            Console.WriteLine(p.Media);
+            Console.WriteLine(p.Mediana);
+            Console.WriteLine(p.Moda);
+            Console.WriteLine(p.Rango);
+        }
+    }
+}
