694f1da99f251bed8c6f8b614f55f3d1d7b3ec35
SamirWawa <samir.wawa@iesdoctorbalmis.com>
2023-04-26 21:50:13 +0200

    Ejercicio4

---
+++ SamirWawa-tema10.2-bloque7/SamirWawa-tema10.2-bloque7-ejercicio4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.RegularExpressions;
+class Polinomio : IEnumerable<SortedDictionary<int, int>>
+{
+    private SortedDictionary<int, int> Monomios { get; set; }
+    public Polinomio(string monomio)
+    {
+        Monomios = new SortedDictionary<int, int>();
+        string patron = @"(?<grupoCoeficiente>[+-]?\d*)?(?<grupoIncognita>[Xx])?(?<grupoExponente>\d*)?";
+        string cadena = "";
+        for (int i = 0; i < monomio.Length; i++)
+        {
+            if (monomio[i + 1] == '+' || monomio[i + 1] == '-' || i == monomio.Length - 1)
+            {
+                Match coincidencia = Regex.Match(cadena, patron);
+                string c = coincidencia.Groups["grupoCoeficiente"].Value;
+                string e = coincidencia.Groups["grupoExponente"].Value;
+                string inc = coincidencia.Groups["grupoIncognita"].Value;
+                if (c == "")
+                    c = "0";
+                if (e == "")
+                    e = "1";
+                if (inc == "")
+                    e = "0";
+                Monomios.Add(int.Parse(e), int.Parse(c));
+                cadena = "";
+            }
+            cadena += monomio[i];
+        }
+    }
+    public IEnumerator<SortedDictionary<int, int>> GetEnumerator() => Monomios.GetEnumerator();
+    System.Collections.IEnumerator
+    System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
+    public static Polinomio Suma(Polinomio p1, Polinomio p2)
+    {
+        foreach (SortedDictionary<int, int> i in p2)
+        {
+            if (p1.Monomios.ContainsKey(i[0]))
+            {
+
+            }
+        }
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Polinomio p = new Polinomio("9x7-3x5-7x-5");
+    }
+}
