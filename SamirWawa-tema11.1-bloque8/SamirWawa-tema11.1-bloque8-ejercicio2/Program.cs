571d16f8cdd50dffb004dacedf8c0aa2b31f6185
SamirWawa <samir.wawa@iesdoctorbalmis.com>
2023-05-03 09:18:04 +0200

    Delegados

---
+++ SamirWawa-tema11.1-bloque8/SamirWawa-tema11.1-bloque8-ejercicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+internal class Program
+{
+    public delegate void Infinitivo();
+    public static void Ser() => Console.WriteLine("be");
+    public static void Correr() => Console.WriteLine("run");
+    public static void Ver() => Console.WriteLine("see");
+    public static void Pensar() => Console.WriteLine("think");
+    public static void Comer() => Console.WriteLine("eat");
+    private static void Main(string[] args)
+    {
+        Infinitivo inf = Ser;
+        inf += Correr;
+        inf += Ver;
+
+        inf();
+
+        inf -= Ser;
+        inf -= Ver;
+        inf += Pensar;
+        inf += Comer;
+
+        inf();
+    }
+}
---
+++ SamirWawa-tema11.1-bloque8/SamirWawa-tema11.1-bloque8-ejercicio3/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    public delegate void Action<T>(T matriz);
+    public static void Muestra<T>(T[][] matriz) 
+    {
+        for(int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for( int j = 0; j < matriz.GetLength(1); j++)
+            {
+                Console.Write(matriz[i][j]);
+            }
+            Console.WriteLine("");
+        }
+    }
+    private static void Main(string[] args)
+    {
+        string[][] b = 
+        {
+            new string[]{"Sal", "Agua", "Azucar", "Vino"},
+            new string[]{"Cola", "Cafe", "Zumo", "Leche"}
+        };
+        
+        
+        Action<string[][]> a = Muestra;
+        a(b);
+    }
+}
